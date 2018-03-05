f312deda6d99aeef2e3ad5adab86211d7147a431
RT Flores <rotyflo@gmail.com>
2018-03-05 17:40:45 -0600

    Add jQuery for brevity and compatibility

--- js/main.js
+++ js/main.js
@@ -26,74 +26,51 @@ for (let i = 0; i < channels.length; i++) {
 //------FUNCTIONALITY------//
 function generateHTML(channel) {
   let url = `https://wind-bow.glitch.me/twitch-api/channels/${channel}`;
-  let request = new XMLHttpRequest();
 
-  request.open("GET", url, true);
-  request.send();
+  $.getJSON(url, function (data) {
+    if (data.hasOwnProperty("error")) invalidChannelHTML(data);
+    else {
+      url = `https://wind-bow.glitch.me/twitch-api/streams/${channel}`;
 
-  request.onload = function () {
-    if (request.status >= 200 && request.status < 400) {
-      let data = JSON.parse(request.responseText);
-
-      if (data.hasOwnProperty("error")) invalidChannelHTML(data);
-      else {
-        url = `https://wind-bow.glitch.me/twitch-api/streams/${channel}`;
-        request = new XMLHttpRequest();
-
-        request.open("GET", url, true);
-        request.send();
-
-        request.onload = function () {
-          if (request.status >= 200 && request.status < 400) {
-            data = JSON.parse(request.responseText);
-
-            if (data.stream === null) offlineChannelHTML(channel);
-            else onlineChannelHTML(data);
-          }
-        }
-
-        request.onerror = function () { };
-      }
+      $.getJSON(url, function (data) {
+        if (data.stream === null) offlineChannelHTML(channel);
+        else onlineChannelHTML(data);
+      });
     }
-  }
-
-  request.onerror = function () { };
+  });
 }
 
 function invalidChannelHTML(data) {
-  let status = data.status;
-  let error = data.error;
-  let message = data.message;
-
   app.innerHTML += `
-    <a class="card span6">
-    <div>  
-    <p>${status}: ${error}</p><p>${message}</p>
+    <div class="card bg-dark border-secondary rounded-0">
+      <p class="text-danger">${data.status}: ${data.error}</p>
+      <p class="text-light">${data.message}</p>
     </div>
-    </a>
   `;
 }
 
 function offlineChannelHTML(channel) {
   app.innerHTML += `
-    <a class="card span6" href="https://www.twitch.tv/${channel}" target="_blank">
-    <div>  
-    <p>${channel} <span class="offline">(offline)</span></p>
-    </div>
+    <a class="card bg-dark text-light border-secondary rounded-0" href="https://www.twitch.tv/${channel}" target="_blank">
+      <div>
+        <p class="text-warning">OFFLINE</p>
+        <p>${channel}</p>
+      </div>
     </a>
   `;
 }
 
 function onlineChannelHTML(data) {
-  let name = data.stream.channel.display_name;
+  let streamer = data.stream.channel.display_name;
   let game = data.stream.game;
   let status = data.stream.channel.status;
   let link = data.stream.channel.url;
 
   app.innerHTML += `
-    <a class="card span6" href="${link}" target="_blank">
+    <a class="card bg-dark text-light border-secondary rounded-0" href="${link}" target="_blank">
       <div>
-        <p>${name} is playing ${game} <span class="online">(online)</span></p>
+        <p class="text-success">ONLINE</p>
+        <p>${streamer} is playing ${game}</p>
         <p>Description: <br>${status}</p>
       </div>
     </a>
